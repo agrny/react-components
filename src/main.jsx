32777029798f9b3158084315351ba9ca238872a5
Alec Greenaway <alecgreenaway1@gmail.com>
2025-02-16 17:19:15 -0800

    navbar works on mobile and acutally links when you click the whole thing, not just when you clicked the link

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TemporaryDrawer from './sideBar.jsx'
+import NavBarRouter from './nav.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TemporaryDrawer />
+    <NavBarRouter />
     <App />
   </StrictMode>,
 )
